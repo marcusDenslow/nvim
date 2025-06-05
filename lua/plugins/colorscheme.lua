d37ba6953eed82bead085159616cd1c1ced35e81
marcusDenslow <marcus.allen.denslow@gmail.com>
2025-06-05 18:46:38 +0200

    bruh

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,28 +1,14 @@
-function ColorMyPencils(color)
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
-end
-
 return {
 	{
-		"rose-pine/neovim",
-		name = "rose-pine",
+		"catppuccin/nvim",
+		name = "catppuccin",
 		priority = 1000,
-		opts = {
-			variant = "auto",
-			dark_variant = "main",
-			disable_background = true,
-			disable_float_background = true,
-			styles = {
-				bold = true,
-				italic = false,
-				transparency = true,
-			},
-		},
-		config = function(_, opts)
-			require("rose-pine").setup(opts)
-			vim.cmd("colorscheme rose-pine")
-			ColorMyPencils()
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha",
+			})
+			
+			vim.cmd("colorscheme catppuccin")
 		end,
 	},
 }
